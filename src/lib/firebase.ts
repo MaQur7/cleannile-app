96cd13f8f42209c7e0b7a73d8b122a058a8109de
Makur Majeng <makurmajengm@gmail.com>
2026-03-04 08:01:55 +0200

    CleanNile production-ready

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuTF4vIw6ZleU65WGdcIhaMqTFc0uoNI0",
-  authDomain: "cleannile-864ad.firebaseapp.com",
-  projectId: "cleannile-864ad",
-  storageBucket: "cleannile-864ad.firebasestorage.app",
-  messagingSenderId: "521489165229",
-  appId: "1:521489165229:web:85a56c1f6988de3410cf24"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
